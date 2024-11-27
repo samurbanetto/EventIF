42c3d87f485ac1026a23a835d25bf72cb1208f03
samurbanetto <101749449+samurbanetto@users.noreply.github.com>
2024-11-27 14:21:33 -0300

    django-extensions settings.py

--- eventif/settings.py
+++ eventif/settings.py
@@ -34,14 +34,15 @@ DEFAULT_FROM_EMAIL = 'contato@eventif.com.br'
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'core',
-    'subscriptions',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django_extensions",
+    "core",
+    "subscriptions",
 ]
 
 MIDDLEWARE = [
