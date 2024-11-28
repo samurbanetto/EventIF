7d000aefdde3058e6d1ec6c16fd84d099f97d570
samurbanetto <101749449+samurbanetto@users.noreply.github.com>
2024-11-28 10:00:02 -0300

    criação do app contacts
    atualização de template index, settings.py, urls e etc

---
+++ contact/form.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    name = forms.CharField(label="Nome")
+    phone = forms.CharField(label="Telefone")
+    email = forms.EmailField(label="Email")
+    message = forms.CharField(label="Mensagem", widget=forms.Textarea(attrs={'name':'body', 'rows':'3', 'cols':'5'}))
