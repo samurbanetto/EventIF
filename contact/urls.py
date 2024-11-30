f5486d272ba323db28757611bcbf7fd46ec5d332
samurbanetto <101749449+samurbanetto@users.noreply.github.com>
2024-11-29 22:22:46 -0300

    Delete contact directory

--- contact/urls.py
+++
@@ -1,8 +0,0 @@
-from contact.views import contact, detail
-from django.urls import path
-
-app_name = 'contact'
-urlpatterns = [
-    path('', contact, name='new'),
-    path('<int:pk>/', detail, name='detail'),
-]
