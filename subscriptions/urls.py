8afcba76d893727ae558c65126022afd46d6ab6f
samurbanetto <101749449+samurbanetto@users.noreply.github.com>
2024-11-29 22:23:31 -0300

    Delete subscriptions directory

--- subscriptions/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from subscriptions.views import subscribe, detail
-
-app_name = "subscriptions"
-urlpatterns = [
-    path("", subscribe, name="new"),
-    path("<int:pk>/", detail, name="detail"),
-]
