582760884db0ec78b48d04278038dc6e9511ac61
Samuel <samuelurbanetto05@gmail.com>
2024-12-01 02:30:05 +0000

    Problemas corrigidos pelo professor no pythonanywhere

--- eventif/urls.py
+++ eventif/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from core.views import home
-from contact.views import contact
 
 urlpatterns = [
     path("", home, name="home"),
